8047c4ac51515ccf79afad4735114e2c0303f485
Emir <emirkpln5402@gmail.com>
2023-05-11 01:19:58 +0300

    Entity Commit

--- src/main/java/com/softito/projectmanagement/Models/Project.java
+++ src/main/java/com/softito/projectmanagement/Models/Project.java
@@ -1,4 +1,22 @@
 package com.softito.projectmanagement.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Projects")
 public class Project {
+
+    @Id
+    public Long Id;
+
+    public String ProjectName;
+    public String ProjectDesc;
+
 }
